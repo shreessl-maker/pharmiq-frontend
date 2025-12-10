7a0edd6c5a60d0e6cd88d2f39c71c0e868c5e8c9
shreessl-maker <shreessl@gmail.com>
2025-12-10 18:02:32 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: { extend: {} },
-  plugins: [],
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#5B21B6",
+        secondary: "#2563EB"
+      }
+    }
+  },
+  plugins: []
 };
